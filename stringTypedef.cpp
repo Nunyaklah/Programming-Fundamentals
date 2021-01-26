12fa1cc1ddedc10a2b92da17feb7ada2b63fb0f8
--global <nunyaklah@gmail.com>
2021-01-26 21:56:02 +0000

    work on typedef

--- stringTypedef.cpp
+++ stringTypedef.cpp
@@ -5,12 +5,22 @@ using namespace std;
 
 int main()
 {
-    typedef string wordsArr[10];
+    typedef string mystring;
 
-    for (int i = 0; i < 11; i++)
-    {
-        cout << "Enter ten strings " << endl;
-
-        cin >> wordsArr[i];
+    mystring arr[10];
+    int arrSize = 0;
+    string longestString;
+    cout<< "Enter ten strings, to see the longest!"<<endl;
+    for(int i =0; i<10; i++){
+        cin>>arr[i];
     }
+    for (int j= 0; j<10; j++){
+        if (arr[j].length() > arrSize ){
+            arrSize = arr[j].length();
+            longestString = arr[j];
+            cout <<longestString;
+        }
+    }
+
+    return 0;
 }
